c4d898a111a1d60635ddf8c5b5d54e93ef329f05
Brooke Chalmers <breq@breq.dev>
2023-12-09 01:58:12 -0500

    wip wip wip

--- day5/part2/solution.py
+++ day5/part2/solution.py
@@ -1,5 +1,4 @@
 import typing
-import itertools
 
 Mapping = typing.NamedTuple(
     "Mapping", [("from_start", int), ("to_start", int), ("length", int)]
@@ -33,23 +32,79 @@ def parse_input():
 seeds, maps = parse_input()
 
 
-def perform_mapping_reversed(map: set[Mapping], value: int):
+def compute_intersection(intervals: set[tuple[int, int]]) -> list[tuple[int, int]]:
+    "Given a set of intervals, return an ordered list of non-overlapping intervals."
+    intersection: list[tuple[int, int]] = []
+    for interval in intervals:
+        # find the index of the first intersection-interval with an end greater than this one's start
+        for i, candidate in enumerate(intersection):
+            if candidate[1] >= interval[0]:
+                first_overlap = i
+                break
+        else:
+            first_overlap = None
+
+        # find the index of the last intersection-interval with a start less than this one's end
+        for i, candidate in reversed(list(enumerate(intersection))):
+            if candidate[0] <= interval[1]:
+                last_overlap = i
+                break
+        else:
+            last_overlap = None
+
+        if first_overlap is None:
+            # this is to the left of all existing intervals, and does not overlap
+            intersection.insert(0, interval)
+        elif last_overlap is None:
+            # this is to the right of all existing intervals, and does not overlap
+            intersection.append(interval)
+        elif last_overlap < first_overlap:
+            # no intervals overlap this interval
+            # insert it after last_overlap
+            assert last_overlap + 1 == first_overlap
+            intersection.insert(last_overlap + 1, interval)
+        else:
+            # we are overlapping intervals and need to merge them
+            # this also handles if we overlap a single interval and extend it (i.e., first_overlap == last_overlap)
+            new_min = min(intersection[first_overlap][0], interval[0])
+            new_max = max(intersection[last_overlap][1], interval[1])
+
+            # extend the first interval
+            intersection[first_overlap] = (new_min, new_max)
+            intersection = (
+                intersection[: first_overlap + 1] + intersection[last_overlap:]
+            )
+
+    return intersection
+
+
+def map_range(map: set[Mapping], start: int, end: int):
+    ranges = set()
     for mapping in map:
-        if value >= mapping.to_start and value < mapping.to_start + mapping.length:
-            offset = value - mapping.to_start
-            return mapping.from_start + offset
+        mapped_start = mapping.to_start + (start - mapping.from_start)
+        mapped_end = mapping.to_start + (end - mapping.from_start)
+        if mapping.from_start <= start <= end <= mapping.from_start + mapping.length:
+            # range entirely contained within mapping
+            ranges.add((mapped_start, mapped_end))
+        elif mapping.from_start <= start <= mapping.from_start + mapping.length <= end:
+            # range extends to the right of the mapping
+            ranges.add((mapped_start, mapping.to_start + mapping.length))
+        elif start <= mapping.from_start <= end <= mapping.from_start + mapping.length:
+            # range extends to the left of the mapping
+            ranges.add((mapping.to_start, mapped_end))
+    # TODO: what about the mapping not in range?
 
-    return value
+    return ranges
 
 
-def perform_mappings_reversed(maps: list[set[Mapping]], value: int):
-    for map in reversed(maps):
-        value = perform_mapping_reversed(map, value)
-    return value
+def perform_mappings(maps: list[set[Mapping]], start: int, end: int):
+    ranges = [(start, end)]
+    for map in maps:
+        new_ranges = []
+        for range in ranges:
+            new_ranges += map_range(map, range[0], range[1])
+        ranges = new_ranges
+    return ranges
 
 
-for location in itertools.count():
-    maybe_seed = perform_mappings_reversed(maps, location)
-    if maybe_seed in seeds:
-        print(location)
-        break
+print(perform_mappings(maps, 79, 79 + 14))
