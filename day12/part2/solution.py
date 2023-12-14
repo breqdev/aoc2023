1e0ee8e09476c2c680da95d2b3144735cae943cf
Brooke Chalmers <breq@breq.dev>
2023-12-14 17:47:28 -0500

    day12 ez pz

---
+++ day12/part2/solution.py
@@ -0,0 +1,74 @@
+import functools
+
+lines: list[tuple[str, tuple[int, ...]]] = []
+
+with open("input.txt") as f:
+    for line in f.readlines():
+        springs_line, groups_line = line.strip().split(" ")
+        groups = tuple(int(i) for i in groups_line.split(","))
+        lines.append((springs_line, groups))
+
+
+def unfold(line: str, groups: tuple[int, ...]):
+    line_result = "?".join(line for _ in range(5))
+    groups_result = groups * 5
+    return line_result, groups_result
+
+
+lines = [unfold(line, groups) for line, groups in lines]
+
+
+@functools.cache
+def solve_line(
+    line: str, groups: tuple[int], remaining_in_group: int = 0, in_group=False
+) -> int:
+    if len(line) == 0:
+        if len(groups) > 0:
+            return 0
+        if remaining_in_group > 0:
+            return 0
+
+        return 1
+
+    if remaining_in_group > 0:
+        if line[0] == ".":
+            return 0
+        elif line[0] == "#":
+            return solve_line(line[1:], groups, remaining_in_group - 1, in_group=True)
+        elif line[0] == "?":
+            # we need to use it
+            return solve_line(line[1:], groups, remaining_in_group - 1, in_group=True)
+        else:
+            raise ValueError(line[0])
+    elif in_group:
+        # we are immediately after a group, so this needs to be working
+        if line[0] == ".":
+            return solve_line(line[1:], groups, in_group=False)
+        elif line[0] == "#":
+            return 0
+        elif line[0] == "?":
+            # this needs to be working
+            return solve_line(line[1:], groups, in_group=False)
+        else:
+            raise ValueError(line[0])
+    else:
+        if line[0] == ".":
+            return solve_line(line[1:], groups, in_group=False)
+        elif line[0] == "#":
+            # we need to use it
+            if len(groups) < 1:
+                return 0
+            return solve_line(line[1:], groups[1:], groups[0] - 1, in_group=True)
+        elif line[0] == "?":
+            if_working = solve_line(line[1:], groups, in_group=False)
+            if_broken = (
+                solve_line(line[1:], groups[1:], groups[0] - 1, in_group=True)
+                if len(groups) >= 1
+                else 0
+            )
+            return if_working + if_broken
+        else:
+            raise ValueError(line[0])
+
+
+print(sum(solve_line(*line) for line in lines))
